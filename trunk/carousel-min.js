42bb3c766af7634eea740462aa9b51bde82d9e8c
vic.stanciu <vic.stanciu@faf31882-30d9-11de-bd8d-4b29057232bf>
2009-04-24 17:35:55 +0000

    initial commit

---
+++ trunk/carousel-min.js
@@ -0,0 +1,25 @@
+/*
+Copyright (c) 2009 Victor Stanciu - http://www.victorstanciu.ro
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('1w=13.14(1x,{15:8(c,d,e,f){4.r=s;4.i=$(c);4.7=d;4.k=e;4.5=C.D({o:1,16:3,p:1,17:\'E-1y\',18:\'E-1z\',F:\'E-1A\',G:\'E-1B\',q:s,t:\'19\',O:\'P\',1a:s},f||{});6(4.5.t==\'H\'){4.5.q=1b}4.7.1C(8(a,b){a.I=b});6(4.k){4.k.J(\'1D\',\'Q\',4.Q.u(4))}6(4.5.1a){4.v()}6(4.5.1c){9 g=4.7.1E($(4.5.1c));6(g>(4.5.p-1)&&4.5.p>1){6(g>4.7.h-(4.5.p+1)){g=4.7.h-4.5.p}}4.l(4.7[g])}},Q:8(a){4.K();9 b=a.1F(\'a\');6(!b.R(4.5.F)){6(b.R(4.5.17)){1G("4."+b.1d+"()")}w 6(b.R(4.5.18)){4.l(b.1d);6(4.5.G){4.k.J(\'1e\',4.5.G);b.1f(4.5.G)}}}4.1g();a.K()},l:8(a){6(4.5.S&&(1h 4.5.S==\'8\')){4.5.S()}4.T=4.m?4.m:4.7[0];4.m=$(a);9 b=4.i.1i();9 c=4.m.1i();6(4.r){4.r.1H()}1j(4.5.t){L\'H\':4.r=U n.1k(4.i,{1l:1.0,1m:0,o:4.5.o,V:(8(){4.i.z=c[0]-b[0];4.i.A=c[1]-b[1];U n.1k(4.i,{1l:0,1m:1.0,o:4.5.o,V:(8(){6(4.k){4.M()}}).u(4)})}).u(4)});N;L\'19\':1n:9 d;1j(4.5.O){L\'1o\':d=n.1p.1o;N;L\'P\':1n:d=n.1p.P;N}4.r=U n.W(4.i,{o:4.5.o,x:(c[0]-b[0]),y:(c[1]-b[1]),O:d,V:(8(){6(4.k){4.M()}4.r=s}).u(4)});N}6(4.5.X&&(1h 4.5.X==\'8\')){4.5.X()}Y s},1I:8(){6(4.m){9 a=4.m.I;9 b=(a==0)?(4.5.q?4.7.h-1:0):a-1}w{9 b=(4.5.q?4.7.h-1:0)}6(b==(4.7.h-1)&&4.5.q&&4.5.t!=\'H\'){4.i.z=(4.7.h-1)*4.7.Z().1J();4.i.A=(4.7.h-1)*4.7.Z().1K();b=4.7.h-2}4.l(4.7[b])},1q:8(){6(4.m){9 a=4.m.I;9 b=(4.7.h-1==a)?(4.5.q?0:a):a+1}w{9 b=1}6(b==0&&4.5.q&&4.5.t!=\'H\'){4.i.z=0;4.i.A=0;b=1}6(b>4.7.h-(4.5.p+1)){b=4.7.h-4.5.p}4.l(4.7[b])},Z:8(){4.l(4.7[0])},1L:8(){4.l(4.7[4.7.h-1])},1M:8(){6(4.T){4.l(4.7[4.T.I])}w{Y s}},K:8(){6(4.B){1r(4.B)}},v:8(){4.10()},1N:8(){4.K();4.M()},1O:8(b){6(b){9 c=b.1P||b.1Q;6(!c||(!4.7.1R(c)&&!4.7.1S(8(a){Y c.1T(a)}))){4.v()}}w{4.v()}},10:8(){6(4.B!=1U){1r(4.B);4.1q()}4.B=1V(4.10.u(4),4.5.16*1W)},1g:8(){4.k.J(\'1f\',4.5.F)},M:8(){4.k.J(\'1e\',4.5.F)}});n.W=13.14();C.D(C.D(n.W.1s,n.1X.1s),{15:8(a){4.j=$(a);9 b=C.D({x:0,y:0,1t:\'1u\'},1Y[1]||{});4.v(b)},1Z:8(){6(4.5.20&&!4.j.1v){4.j.21();4.j.1v=1b;4.j.22(4.j.23)}4.11=4.j.z;4.12=4.j.A;6(4.5.1t==\'1u\'){4.5.x-=4.11;4.5.y-=4.12}},24:8(a){4.j.z=4.5.x*a+4.11;4.j.A=4.5.y*a+4.12}});',62,129,'||||this|options|if|slides|function|var||||||||length|scroller|element|controls|moveTo|current|Effect|duration|visibleSlides|circular|scrolling|false|effect|bind|start|else|||scrollLeft|scrollTop|timer|Object|extend|carousel|disabledClassName|selectedClassName|fade|_index|invoke|stop|case|activateControls|break|transition|sinoidal|click|hasClassName|beforeMove|previous|new|afterFinish|SmoothScroll|afterMove|return|first|periodicallyUpdate|originalLeft|originalTop|Class|create|initialize|frequency|controlClassName|jumperClassName|scroll|auto|true|initial|rel|removeClassName|addClassName|deactivateControls|typeof|cumulativeOffset|switch|Opacity|from|to|default|spring|Transitions|next|clearTimeout|prototype|mode|absolute|_ext|Carousel|Abstract|control|jumper|disabled|selected|each|observe|indexOf|findElement|eval|cancel|prev|getWidth|getHeight|last|toggle|pause|resume|relatedTarget|toElement|include|any|descendantOf|null|setTimeout|1000|Base|arguments|setup|continuous|cleanWhitespace|appendChild|firstChild|update'.split('|'),0,{}))
